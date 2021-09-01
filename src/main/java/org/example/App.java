10cb15743c1e8922e4782b2d209ee14f4937613e
patpatdotmp4 <47404725+patpatdotmp4@users.noreply.github.com>
2021-08-31 20:41:08 -0400

    made conversion a constant

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,8 +5,10 @@ import java.util.Scanner;
  *  UCF COP3330 Fall 2021 Assignment 1 Exercise 7 Solution
  *  Copyright 2021 Patrick Mac
  */
+
 public class App
 {
+    private static final double n = 0.09290304;
     public static void main( String[] args )
     {
         Scanner scan = new Scanner(System.in);
@@ -16,8 +18,11 @@ public class App
         String stringWidth = scan.nextLine();
         double length=Double.parseDouble(stringLength);
         double width=Double.parseDouble(stringWidth);
+
+
         double f2 = length * width;
-        double m2 = f2 * 0.09290304;
+        double m2 = f2 * n;
+
         System.out.println("You entered dimensions of "+stringLength+" feet by "+stringWidth+" feet.");
         System.out.println("The area is");
         System.out.println(f2+" square feet");
